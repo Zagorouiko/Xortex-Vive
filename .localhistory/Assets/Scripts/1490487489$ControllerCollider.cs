0537713892e96fdfa0ce43b0df20f650393d85a5
Zagorouiko <Zagorouiko@gmail.com>
2017-03-25 17:05:49 -0700

    add camera rig and attach controller colliders to controllers.

---
+++ .localhistory/Assets/Scripts/1490487489$ControllerCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCollider : MonoBehaviour
+{
+
+    public SteamVR_TrackedObject controller;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
